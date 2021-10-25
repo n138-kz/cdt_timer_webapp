ddb373a0382a79a77c764c1f15904efadbe1ef22
Yuu Takanashi <n01@n138.nws>
2021-10-25 18:30:11 +0900

    path fix (service worker)

--- html/serviceWorker.js
+++ html/serviceWorker.js
@@ -1,6 +1,6 @@
 var CACHE_NAME  = "cache-v1";
 var urlsToCache = [
-  "/index.html",
+  "/cdt_timer_webapp/index.html",
   "https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css",
   "https://code.jquery.com/jquery-3.5.1.min.js",
   "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js",
